49a1a6b5d4e71b50342c08f918c886a6033b5944
HrvojeCutvaric <111574558+HrvojeCutvaric@users.noreply.github.com>
2026-02-04 15:35:34 +0100

    Add custom Material3 theme with custom colors and typography

--- composeApp/src/commonMain/kotlin/co/hrvoje/drawshapesandroid/App.kt
+++ composeApp/src/commonMain/kotlin/co/hrvoje/drawshapesandroid/App.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import co.hrvoje.drawshapesandroid.ui.DrawShapeTheme
 import org.jetbrains.compose.resources.painterResource
 
 import drawshapesandroid.composeapp.generated.resources.Res
@@ -22,7 +23,7 @@ import drawshapesandroid.composeapp.generated.resources.compose_multiplatform
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+    DrawShapeTheme {
         var showContent by remember { mutableStateOf(false) }
         Column(
             modifier = Modifier
---
+++ composeApp/src/commonMain/kotlin/co/hrvoje/drawshapesandroid/ui/Typography.kt
@@ -0,0 +1,15 @@
+package co.hrvoje.drawshapesandroid.ui
+
+import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun typography(): Typography = Typography(
+    titleLarge = TextStyle(
+        fontSize = 24.sp,
+        fontWeight = FontWeight.ExtraBold,
+    ),
+)
