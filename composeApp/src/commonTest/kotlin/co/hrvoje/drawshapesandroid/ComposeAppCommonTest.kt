2c0b6890af4399f2497ad86e812bd231b679cb6f
HrvojeCutvaric <111574558+HrvojeCutvaric@users.noreply.github.com>
2026-02-04 15:30:53 +0100

    project init

---
+++ composeApp/src/commonTest/kotlin/co/hrvoje/drawshapesandroid/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package co.hrvoje.drawshapesandroid
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
